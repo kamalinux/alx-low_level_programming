884f11d7e67fdbdbb7f8c8ad23f2fb80d6ce5e1a
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-03 16:09:34 -0700

    added 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
