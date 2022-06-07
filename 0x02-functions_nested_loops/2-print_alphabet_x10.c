75d4fabb48c8ee461d01d0f5cab23adfa5b5fa6e
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-07 15:09:58 -0700

    added 2-print_alphabet_x10.c file 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,12 @@ void print_alphabet_x10(void)
 {
 	char alpha;
 	int count = 0;
+
 	while (count < 10)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
-		
+
 		count++;
 		_putchar('\n');
 	}
