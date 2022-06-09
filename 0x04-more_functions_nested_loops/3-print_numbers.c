a23534e539cb69e20a9dfa3d3b2c3570da779bff
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-09 16:54:59 -0700

    added 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - _putchar the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always (Success)
+ */
+
+void print_numbers(void)
+{
+	_putchar('0');
+	_putchar('1');
+	_putchar('2');
+	_putchar('3');
+	_putchar('4');
+	_putchar('5');
+	_putchar('6');
+	_putchar('7');
+	_putchar('8');
+	_putchar('9');
+	_putchar('\n');
+}
