5abc4271c536f57dcb8c17adee386669b2425d57
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-13 17:10:33 -0700

    added 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of the string
+ *@s: string being counted
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
