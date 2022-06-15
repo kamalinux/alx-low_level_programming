0057e61e090bd57c8e9707dca54fa95577d141b3
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-15 07:17:56 -0700

    added 3-strcmp.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <string.h>
 
-
+/**
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
