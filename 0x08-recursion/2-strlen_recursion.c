8561eed50c66a20d64c019a048ea429a6d794152
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-22 14:42:04 -0700

    added 2-strlen_recursion.c file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
