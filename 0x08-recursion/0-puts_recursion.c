636cdf8c27d8f9a63cfd423ba829763d92721fa5
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-22 14:17:02 -0700

    added 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
