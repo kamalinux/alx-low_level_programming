0d750644940acf784bc59efdb375fd566e3c9e70
Kamal Hussein <kamalinuxx@gmail.com>
2022-06-24 15:49:32 -0700

    added 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Program that adds two numbers
+ *@argc: count of arguments
+ *@argv: array of pointers to the strings passed
+ * Return: Always 0, return 1 if an error
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0, a, b;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[a]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
