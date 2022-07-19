62ce50c579ad761cf7fe0d574789f2eb4fd46dbb
Kamal Hussein <kamalinuxx@gmail.com>
2022-07-19 04:12:14 -0700

    fixing 10-delete_nodeint.c files

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,47 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes a node at a certain index
- * @head: pointer to head of node
- * @index: Index of the node
- * Return: Always 0.
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old, *next;
 	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-	if (*head == NULL || head == NULL)
+	prev = *head;
+
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
+
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
 		return (-1);
-	next = *head;
+	}
 
-	if (index == 0)
+	next = prev->next;
+
+	if (index != 0)
 	{
-		old = next->next;
+		prev->next = next->next;
 		free(next);
-		*head = old;
-		return (1);
 	}
-	for (i = 0; i < index; i++)
+	else
 	{
-		if (next == NULL)
-			return (-1);
-		old = next;
-		next = next->next;
+		free(prev);
+		*head = next;
 	}
-	old->next = next->next;
-	free(next);
-	if (index == 0)
-	{
-		head = &old;
-		return (1);
-	}
-	return (i);
+
+	return (1);
 }
