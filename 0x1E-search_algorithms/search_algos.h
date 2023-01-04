fcbdbd766dc9beda2bca46660d504feb268c2fda
Kamal Hussein <kamalinuxx@gmail.com>
2023-01-04 15:16:33 -0800

    Adding a function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,5 @@ int recursive_search(int *array, size_t size, int value);
 
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
